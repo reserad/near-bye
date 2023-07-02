3e0467493b455ca8c4c4b943438795a8c5153f0b
Alec Reser <reser13@gmail.com>
2023-07-01 23:51:51 -0400

    Slightly styled account screen

--- src/modules/account/accountContainer.tsx
+++ src/modules/account/accountContainer.tsx
@@ -1,9 +1,10 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Screen } from "../../components/Screen/screen";
 import { Button } from "../../components/Button/button";
 import { useNewDispatch } from "../../store/hooks/useNewDispatch";
 import { signOut } from "../../store/user/actions/signOut";
 import { BottomTabStackProps } from "../../navigation/types";
+import { Theme } from "../../shared/theme";
 
 export const AccountContainer = ({
   navigation,
@@ -15,9 +16,32 @@ export const AccountContainer = ({
   };
   return (
     <Screen scroll>
-      <View>
+      <View style={styles.container}>
+        <View style={styles.profilePictureContainer}>
+          <View style={styles.profilePicture} />
+        </View>
         <Button text="Sign out" onPress={handleOnPress} />
       </View>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: Theme.padding.P4,
+  },
+  profilePictureContainer: {
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    marginBottom: Theme.padding.P4,
+  },
+  profilePicture: {
+    width: Theme.padding.P26,
+    height: Theme.padding.P26,
+    borderRadius: Theme.padding.P13,
+    backgroundColor: Theme.color.white,
+    borderColor: Theme.color.mediumGray,
+    borderWidth: 1,
+  },
+});
